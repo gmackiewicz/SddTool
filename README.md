0fd3e2aaceed76dfbb93cda3ee2345cd5df70c75
Gabriel Mackiewicz <gabriel.mackiewicz@gmail.com>
2018-04-26 18:53:04 +0200

    SddTool

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # SddTool
 Simple tool converting base64 string to image. Especially useful in Screenshot Driven Development approach.
+
+Icons made by [Smashicons](https://www.flaticon.com/authors/smashicons "Smashicons") from [www.flaticon.com](https://www.flaticon.com/ "Flaticon") is licensed by [CC 3.0 BY](http://creativecommons.org/licenses/by/3.0/ "Creative Commons BY 3.0")
---
+++ SddTool/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+
+namespace SddTool
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var input = Base64Input.Text;
+            byte[] data = Convert.FromBase64String(input);
+
+            var directory = Path.Combine(Directory.GetCurrentDirectory(), "converted");
+            Directory.CreateDirectory(directory);
+
+            var filePath = Path.Combine(directory, $"{DateTime.Now.Ticks}.jpg");
+            using (var imageFile = new FileStream(filePath, FileMode.Create))
+            {
+                imageFile.Write(data, 0, data.Length);
+                imageFile.Flush();
+            }
+
+            Process.Start(directory);
+
+            Close();
+        }
+    }
+}
